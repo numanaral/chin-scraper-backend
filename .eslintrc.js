731f1455bfe677157d9f5bf1db98adfbd31c6763
numanaral <ahmet.aral@ucalgary.ca>
2020-11-02 09:19:22 -0700

    utility config & package setup

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
+module.exports = {
+	env: {
+		node: true,
+		commonjs: true,
+		es2021: true,
+	},
+	extends: ['airbnb-base', 'plugin:prettier/recommended'],
+	parserOptions: {
+		ecmaVersion: 12,
+	},
+	plugins: ['prettier'],
+	// settings: {
+	// 	'import/resolver': {
+	// 		node: {
+	// 			extensions: ['.js'],
+	// 			paths: ['./src'],
+	// 		},
+	// 	},
+	// },
+	rules: {
+		'arrow-parens': [ERROR, 'as-needed'],
+		'default-case': [ERROR, { commentPattern: 'No Default' }],
+		'import/no-named-as-default': OFF,
+		'import/prefer-default-export': OFF,
+		'linebreak-style': [ERROR, 'windows'],
+		'max-len': [
+			ERROR,
+			{
+				ignoreComments: false,
+				ignoreUrls: true,
+				ignoreRegExpLiterals: true,
+				ignoreTrailingComments: true,
+				ignoreTemplateLiterals: true,
+			},
+		], // *
+		'no-alert': WARN,
+		'no-console': OFF,
+		'no-continue': OFF,
+		'no-plusplus': OFF,
+		'no-tabs': OFF,
+		'no-underscore-dangle': OFF,
+		'prettier/prettier': ERROR,
+	},
+};
