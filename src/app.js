c8d89b1ef36cf61eddc3d97181f509fe08b6636a
numanaral <ahmet.aral@ucalgary.ca>
2020-11-02 09:24:38 -0700

    limit origins that can access the app through cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,24 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors());
+app.use(
+	cors({
+		credentials: true,
+		methods: 'GET',
+		origin: (origin, callback) => {
+			// allow requests with no origin
+			// (like mobile apps or curl requests)
+			if (!origin) return callback(null, true);
+			if (process.env.ALLOWED_ORIGINS.split(',').indexOf(origin) === -1) {
+				const msg =
+					'The CORS policy for this site does not ' +
+					'allow access from the specified Origin.';
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		},
+	})
+);
 app.use(express.json());
 
 app.get('/', (req, res) => {
