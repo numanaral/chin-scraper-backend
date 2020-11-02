57a37825e3b0fe3f2a433b0a1d8bf0badbfc646f
numanaral <ahmet.aral@ucalgary.ca>
2020-11-02 09:46:03 -0700

    util to generate error based on env

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+/**
+ * Generates an error message based on the env and custom messages
+ * @param {Error} err - The error caught
+ * @param {String} userMessage - Message that prod user see
+ * @param {String} devMessage - Message that dev sees on top of the error caught
+ */
+const generateError = (err, userMessage, devMessage = '') => {
+	const message =
+		(process.env.NODE_ENV === 'production' && userMessage) ||
+		`${
+			(!devMessage && '') ||
+			`${devMessage}${(!devMessage.endsWith('.') && '.') || ''} `
+		}${err.message}`;
+	return new Error(message);
+};
+
+module.exports = {
+	generateError,
+};
