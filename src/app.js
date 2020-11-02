46192feb640ce1f51ec65fc6d7741c47ac898015
numanaral <ahmet.aral@ucalgary.ca>
2020-11-02 09:29:55 -0700

    middleware to prevent URL/browser access on prod

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ app.get('/', (req, res) => {
 	});
 });
 
+app.use(middleware.checkRequiredHeaders);
 app.use(middleware.notFound);
 app.use(middleware.errorHandler);
 
--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,31 @@
 /**
+ * Makes sure to prevent URL/browser access on prod
+ *
+ * @see https://github.com/Rob--W/cors-anywhere/blob/528ad7109fa55bdde8055dd035bb16140a29c336/lib/cors-anywhere.js#L290
+ *
+ * @param {import("express").Request} req - Request
+ * @param {import("express").Response} res - Response
+ * @param {import("express").NextFunction} next - Next
+ */
+const checkRequiredHeaders = (req, res, next) => {
+	let error;
+	if (process.env.NODE_ENV === 'production') {
+		const hasRequiredHeaders = [
+			'origin',
+			'x-requested-with',
+		].some(headerName =>
+			Object.hasOwnProperty.call(req.headers, headerName)
+		);
+		if (!hasRequiredHeaders) {
+			error = new Error(
+				"You don't have the required headers to make a request."
+			);
+		}
+	}
+	next(error);
+};
+
+/**
  * Handles not found
  *
  * @param {import("express").Request} req - Request
@@ -31,6 +58,7 @@ const errorHandler = (err, req, res, next) => {
 };
 
 module.exports = {
+	checkRequiredHeaders,
 	notFound,
 	errorHandler,
 };
